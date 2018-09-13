6e6155b3c5f72c93a266a99ba80d057ae97c514b
HvrlK <g.v.s.23.07@gmail.com>
2018-09-13 21:48:56 +0300

    Created TextViewController.

---
+++ ApiECHO/TextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextViewController.swift
+//  ApiECHO
+//
+//  Created by Vitalii Havryliuk on 9/13/18.
+//  Copyright © 2018 Vitalii Havryliuk. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    var accessToken: String?
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    //MARK: - Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
