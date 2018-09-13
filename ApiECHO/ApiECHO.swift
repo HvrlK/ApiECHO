df28dd5626d82487798463cc56d7d6f14c814a89
HvrlK <g.v.s.23.07@gmail.com>
2018-09-13 18:10:25 +0300

    Added method	for logging.

--- ApiECHO/ApiECHO.swift
+++ ApiECHO/ApiECHO.swift
@@ -39,4 +39,26 @@ class ApiECHO {
     }
     
     
+    func login(email: String, password: String, completion: @escaping (_ accessToken: String?, _ errorMessage: String?) -> Void) {
+        Alamofire.request("https://apiecho.cf/api/login/",
+                          method: .post,
+                          parameters: ["email": email,
+                                       "password": password])
+            .responseJSON { response in
+                guard response.result.isSuccess, let value = response.result.value else {
+                    print("Error while login: \(String(describing: response.result.error))")
+                    completion(nil, "Error while login")
+                    return
+                }
+                guard JSON(value)["success"] == true else {
+                    let errorMessage = JSON(value)["errors"][0]["message"].stringValue
+                    completion(nil, errorMessage)
+                    return
+                }
+                let accessToken = JSON(value)["data"]["access_token"].stringValue
+                completion(accessToken, nil)
+        }
+    }
+
+    
 }
